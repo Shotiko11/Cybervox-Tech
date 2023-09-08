0d9c5edb28a3fa717b6d3c27a1afd5bd7555873e
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-09-08 15:59:34 +0400

    next commit

--- src/app/about/AboutPage.js
+++ src/app/about/AboutPage.js
@@ -1,4 +1,5 @@
-import "./aboutPage.css";
+import Image from "next/image";
+import "./aboutPage.css"; // Import your CSS file
 import React from "react";
 
 const AboutPage = () => {
@@ -15,7 +16,7 @@ const AboutPage = () => {
         </p>
         <p className="company-description">
           At Cybervox Tech, we believe in experience, our knowledge, and your
-          buisness success. These values drive us to our main goal.
+          business success. These values drive us to our main goal.
         </p>
       </div>
       <div className="history-side">
@@ -28,8 +29,83 @@ const AboutPage = () => {
             reach, and receiving industry recognition.
           </p>
           <p className="company-description">
-            Our commitment to innovation and customer-centricity has driven our success and positioned us as a leader in the technology industry.
+            Our commitment to innovation and customer-centricity has driven our
+            success and positioned us as a leader in the technology industry.
           </p>
+        </div>
+      </div>
+      <div className="team-section">
+        <div className="about-container">
+          <h2 className="section-heading">Introduce Our Team</h2>
+          <div className="team-members">
+            {/* Team Member 1 */}
+            <div className="team-member">
+              <iImage
+                src="/images/personone.jpg"
+                alt="Lasha"
+                className="team-member-photo"
+                width={200}
+                height={160}
+              />
+              <h3 className="team-member-name">Lasha</h3>
+              <p className="team-member-title">CEO</p>
+              <p className="team-member-description">
+                Lasha is a visionary leader with a passion for innovation and
+                technology. He leads our team towards excellence.
+              </p>
+            </div>
+
+            {/* Team Member 2 */}
+            <div className="team-member">
+              <Image
+                src="/images/personone.jpg"
+                alt="Nika"
+                className="team-member-photo"
+                width={200}
+                height={160}
+              />
+              <h3 className="team-member-name">Nika</h3>
+              <p className="team-member-title">CTO</p>
+              <p className="team-member-description">
+                Nika is our technical genius who drives our cutting-edge
+                solutions. His expertise is unmatched.
+              </p>
+            </div>
+
+            {/* Team Member 3 */}
+            <div className="team-member">
+              <Image
+                src="/images/personone.jpg"
+                alt="Ani"
+                className="team-member-photo"
+                width={200}
+                height={160}
+              />
+              <h3 className="team-member-name">Ani</h3>
+              <p className="team-member-title">Lead Designer</p>
+              <p className="team-member-description">
+                {"Ani's"} creativity knows no bounds. She crafts stunning designs
+                that leave a lasting impression.
+              </p>
+            </div>
+
+            {/* Team Member 4 */}
+            <div className="team-member">
+              <Image
+                src="/images/personone.jpg"
+                alt="Naina"
+                className="team-member-photo"
+                width={200}
+                height={160}
+              />
+              <h3 className="team-member-name">Naina</h3>
+              <p className="team-member-title">Marketing Manager</p>
+              <p className="team-member-description">
+                Naina is our marketing guru. She knows how to reach our audience
+                and build strong relationships.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>

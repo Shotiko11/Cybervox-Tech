6de5e98d7280f4d384d17aa501203906704f1ef2
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-09-12 10:04:43 +0400

    next commit

--- src/app/contact/ContactPage.js
+++ src/app/contact/ContactPage.js
@@ -1,39 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./contactPage.css";
 import { sectionHeading, contactInfo, contactForm, locationMap, businessHours, socialMedia } from "./Element";
+import useContactPageLogic from "./Functions";
+
+
 
 const ContactPage = () => {
-  const [text, setText] = useState(false);
-  const [message, setMessage] = useState("");
-  const [showEmptyMessage, setShowEmptyMessage] = useState(false);
-  const [countdown, setCountdown] = useState(5);
 
-  useEffect(() => {
-    let timer;
-    if (countdown > 0 && text) {
-      timer = setInterval(() => {
-        setCountdown((prevCountdown) => prevCountdown - 1);
-      }, 1000);
-    } else if (countdown === 0) {
-      clearInterval(timer);
-      setCountdown(5);
-      setText(false);
-    }
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, [countdown, text]);
-
-  const handleClick = () => {
-    if (message.trim() === "") {
-      setShowEmptyMessage(true);
-    } else {
-      setText(true);
-      setShowEmptyMessage(false);
-      setCountdown(5);
-    }
-  };
+  const {
+    text,
+    message,
+    showEmptyMessage,
+    countdown,
+    handleClick
+  } = useContactPageLogic();
 
   return (
     <div className="contact-section">
@@ -47,6 +27,7 @@ const ContactPage = () => {
       </div>
     </div>
   );
+
 };
 
 export default ContactPage;

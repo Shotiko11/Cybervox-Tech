eafcf51c04ca0573eb1e1daa66123126b72ba999
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-09-12 15:16:07 +0400

    next commit

--- src/app/register/RegisterForm.js
+++ src/app/register/RegisterForm.js
@@ -1,36 +1,45 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import './registerForm.css'
+
 
 const RegisterForm = () => {
   return (
-    <div className={styles.container}>
-      <form className={styles.loginForm}>
+    <div className="container">
+      <form className="registrationForm">
         <h2>Register</h2>
-        <div className={styles.formGroup}>
+        <div className="formGroup">
           <label htmlFor="fullName">Full Name:</label>
           <input type="text" id="fullName" name="fullName" />
         </div>
-        <div className={styles.formGroup}>
+        <div className="formGroup">
           <label htmlFor="email">Email:</label>
           <input type="email" id="email" name="email" />
         </div>
-        <div className={styles.formGroup}>
+        <div className="formGroup">
           <label htmlFor="address">Address:</label>
           <input type="text" id="address" name="address" />
         </div>
-        <div className={styles.formGroup}>
+        <div className="formGroup">
           <label htmlFor="password">Password:</label>
           <input type="password" id="password" name="password" />
         </div>
-        <div className={styles.formGroup}>
+        <div className="formGroup">
           <label htmlFor="repeatPassword">Repeat Password:</label>
           <input type="password" id="repeatPassword" name="repeatPassword" />
         </div>
-        <button className={styles.loginButton} type="submit">
+        <button className="registrationButton" type="submit">
           Register
         </button>
+        <p>
+          Already have an account?{' '}
+          <Link href="/Login">
+            Login here
+          </Link>
+        </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;

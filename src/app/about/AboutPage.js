252b488fc596f80e32369a7d58a91b6a74a2bdd1
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-09-14 14:09:53 +0400

    next commit

--- src/app/about/AboutPage.js
+++ src/app/about/AboutPage.js
@@ -4,177 +4,109 @@ import React, { useState } from "react";
 import { info, text } from "./Infos";
 
 const AboutPage = () => {
-  const [Lasha, setLasha] = useState(false);
-  const [nika, setNika] = useState(false);
-  const [ani, setAni] = useState(false);
-  const [naina, setNaina] = useState(false);
+  const [selectedMember, setSelectedMember] = useState(null);
   const [overview, setOverview] = useState(true);
   const [history, setHistory] = useState(true);
+
+  const teamMembers = [
+    {
+      name: "Lasha",
+      title: "CEO",
+      description:
+        "Lasha is a visionary leader with a passion for innovation and technology. He leads our team towards excellence.",
+      image: "/images/personfour.jpg",
+      infoKey: "first",
+    },
+    {
+      name: "Nika",
+      title: "CTO",
+      description:
+        "Nika is our technical genius who drives our cutting-edge solutions. His expertise is unmatched.",
+      image: "/images/personone.jpg",
+      infoKey: "second",
+    },
+    {
+      name: "Ani",
+      title: "Lead Designer",
+      description:
+        "Ani's creativity knows no bounds. She crafts stunning designs that leave a lasting impression.",
+      image: "/images/personfour.jpg",
+      infoKey: "third",
+    },
+    {
+      name: "Naina",
+      title: "Marketing Manager",
+      description:
+        "Naina is our marketing guru. She knows how to reach our audience and build strong relationships.",
+      image: "/images/personone.jpg",
+      infoKey: "fourth",
+    },
+  ];
+
+  const handleMemberClick = (index) => {
+    setSelectedMember(index);
+  };
 
   return (
     <div className="about-section">
       <div className="about-container">
-        <h2 className="section-heading center-team" onClick={() =>{ setOverview(!overview) }}>Company Overview</h2>
-        {overview ? <p className="company-description">{text.overview.overviewOne}</p> : <p className="company-description">{text.overview.overviewTwo}</p>}
+        <h2 className="section-heading center-team" onClick={() => setOverview(!overview)}>
+          Company Overview
+        </h2>
+        {overview ? (
+          <p className="company-description">{text.overview.overviewOne}</p>
+        ) : (
+          <p className="company-description">{text.overview.overviewTwo}</p>
+        )}
       </div>
       <div className="history-side">
         <div className="about-container">
-          <h2 className="section-heading center-team" onClick={() => { setHistory(!history) }}>Our History</h2>
-          {history ? <p className="company-description">{text.history.historyOne}</p> : <p className="company-description">{text.history.historyTwo}</p>}
+          <h2 className="section-heading center-team" onClick={() => setHistory(!history)}>
+            Our History
+          </h2>
+          {history ? (
+            <p className="company-description">{text.history.historyOne}</p>
+          ) : (
+            <p className="company-description">{text.history.historyTwo}</p>
+          )}
         </div>
       </div>
       <div className="team-section">
         <div className="about-container">
-          <h2
-            className="section-heading center-team"
-            style={{ fontSize: "34px" }}
-          >
+          <h2 className="section-heading center-team" style={{ fontSize: "34px" }}>
             Introduce Our Team
           </h2>
-          <p
-            className="section-heading center-team"
-            style={{ fontSize: "18px" }}
-          >
+          <p className="section-heading center-team" style={{ fontSize: "18px" }}>
             For More Information About Our Team, Click On Their Names
           </p>
           <div className="team-members">
-            {/* Team Member 1 */}
-            <div className="team-member">
-              <Image
-                src="/images/personfour.jpg"
-                alt="Lasha"
-                className="team-member-photo"
-                width={200}
-                height={160}
-              />
-              <h3
-                className="team-member-name"
-                onClick={() => {
-                  setLasha(!Lasha);
-                }}
-              >
-                Lasha
-              </h3>
-              <p className="team-member-title">CEO</p>
-              <p className="team-member-description">
-                Lasha is a visionary leader with a passion for innovation and
-                technology. He leads our team towards excellence.
-              </p>
-            </div>
-
-            {/* Team Member 2 */}
-            <div className="team-member">
-              <Image
-                src="/images/personone.jpg"
-                alt="Nika"
-                className="team-member-photo"
-                width={200}
-                height={160}
-              />
-              <h3
-                className="team-member-name"
-                onClick={() => {
-                  setNika(!nika);
-                }}
-              >
-                Nika
-              </h3>
-              <p className="team-member-title">CTO</p>
-              <p className="team-member-description">
-                Nika is our technical genius who drives our cutting-edge
-                solutions. His expertise is unmatched.
-              </p>
-            </div>
-
-            {/* Team Member 3 */}
-            <div className="team-member">
-              <Image
-                src="/images/personfour.jpg"
-                alt="Ani"
-                className="team-member-photo"
-                width={200}
-                height={160}
-              />
-              <h3
-                className="team-member-name"
-                onClick={() => {
-                  setAni(!ani);
-                }}
-              >
-                Ani
-              </h3>
-              <p className="team-member-title">Lead Designer</p>
-              <p className="team-member-description">
-                {"Ani's"} creativity knows no bounds. She crafts stunning
-                designs that leave a lasting impression.
-              </p>
-            </div>
-
-            {/* Team Member 4 */}
-            <div className="team-member">
-              <Image
-                src="/images/personone.jpg"
-                alt="Naina"
-                className="team-member-photo"
-                width={200}
-                height={160}
-              />
-              <h3
-                className="team-member-name"
-                onClick={() => {
-                  setNaina(!naina);
-                }}
-              >
-                Naina
-              </h3>
-              <p className="team-member-title">Marketing Manager</p>
-              <p className="team-member-description">
-                Naina is our marketing guru. She knows how to reach our audience
-                and build strong relationships.
-              </p>
-            </div>
+            {teamMembers.map((member, index) => (
+              <div className="team-member" key={index}>
+                <Image
+                  src={member.image}
+                  alt={member.name}
+                  className="team-member-photo"
+                  width={200}
+                  height={160}
+                />
+                <h3 className="team-member-name" onClick={() => handleMemberClick(index)}>
+                  {member.name}
+                </h3>
+                <p className="team-member-title">{member.title}</p>
+                {selectedMember === index && (
+                  <div className="custom-container">
+                    <h1 style={{ textAlign: "center" }}>{member.name}</h1>
+                    <div className="custom-child-div">{info[member.infoKey].first}</div>{" "}
+                    <div className="custom-child-div">{info[member.infoKey].second}</div>{" "}
+                    <div className="custom-child-div">{info[member.infoKey].third}</div>{" "}
+                    <div className="custom-child-div">{info[member.infoKey].fourth}</div>{" "}
+                    <div className="custom-child-div">{info[member.infoKey].fifth}</div>{" "}
+                  </div>
+                )}
+              </div>
+            ))}
           </div>
         </div>
-        {Lasha ? (
-          <div className="custom-container">
-            <h1 style={{ textAlign: "center" }}>1. Lasha</h1>
-            <div className="custom-child-div">{info.first.first}</div>{" "}
-            <div className="custom-child-div">{info.first.second}</div>{" "}
-            <div className="custom-child-div">{info.first.third}</div>{" "}
-            <div className="custom-child-div">{info.first.fourth}</div>{" "}
-            <div className="custom-child-div">{info.first.fifth}</div>{" "}
-          </div>
-        ) : null}
-        {nika ? (
-          <div className="custom-container">
-            <h1 style={{ textAlign: "center" }}>2. Nika</h1>
-            <div className="custom-child-div">{info.second.first}</div>{" "}
-            <div className="custom-child-div">{info.second.second}</div>{" "}
-            <div className="custom-child-div">{info.second.third}</div>{" "}
-            <div className="custom-child-div">{info.second.fourth}</div>{" "}
-            <div className="custom-child-div">{info.second.fifth}</div>{" "}
-          </div>
-        ) : null}
-        {ani ? (
-          <div className="custom-container">
-            <h1 style={{ textAlign: "center" }}>3. Ani</h1>
-            <div className="custom-child-div">{info.third.first}</div>{" "}
-            <div className="custom-child-div">{info.third.second}</div>{" "}
-            <div className="custom-child-div">{info.third.third}</div>{" "}
-            <div className="custom-child-div">{info.third.fourth}</div>{" "}
-            <div className="custom-child-div">{info.third.fifth}</div>{" "}
-          </div>
-        ) : null}
-        {naina ? (
-          <div className="custom-container">
-            <h1 style={{ textAlign: "center" }}>4. Naina</h1>
-            <div className="custom-child-div">{info.fourth.first}</div>{" "}
-            <div className="custom-child-div">{info.fourth.second}</div>{" "}
-            <div className="custom-child-div">{info.fourth.third}</div>{" "}
-            <div className="custom-child-div">{info.fourth.fourth}</div>{" "}
-            <div className="custom-child-div">{info.fourth.fifth}</div>{" "}
-          </div>
-        ) : null}
       </div>
     </div>
   );

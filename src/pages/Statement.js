b31d46f66b977fff2f620535bffd3e4692456502
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-09-20 14:03:46 +0400

    next commit

--- src/pages/Statement.js
+++ src/pages/Statement.js
@@ -1,10 +1,11 @@
 import LoginForm from '@/app/login/LoginForm'
+import StatementPage from '@/app/statement/StatementPage'
 import React from 'react'
 
 const Login = () => {
   return (
     <div>
-      <LoginForm />
+      <StatementPage />
     </div>
   )
 }

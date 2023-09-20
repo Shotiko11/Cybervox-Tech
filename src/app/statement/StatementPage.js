ca57b14b52bf8c0d3eac8760126b9bf106cd0461
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-09-20 14:45:46 +0400

    next commit

--- src/app/statement/StatementPage.js
+++ src/app/statement/StatementPage.js
@@ -6,25 +6,48 @@ const StatementPage = () => {
   return (
     <div>
       <Header />
-      <div className="page-content"> {/* Added class "page-content" */}
-        <h1>Get Support for Your Company</h1>
-        <p>
+      <div className="page-content">
+        <h1 className="page-title">Get Support for Your Company</h1>
+        <p className="page-description">
           Fill out the form below to let us know how we can assist you. Whether
           you need IT support, programming help, or graphic design services,
           {"we're"} here to help you achieve your goals.
         </p>
-        <form>
+        <form className="request-form">
           <div className="form-group">
-            <label htmlFor="companyName">Company Name:</label>
-            <input type="text" id="companyName" name="companyName" required />
+            <label htmlFor="companyName" className="form-label">
+              Company Name:
+            </label>
+            <input
+              type="text"
+              id="companyName"
+              name="companyName"
+              className="form-input"
+              required
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="fullName">Your Name:</label>
-            <input type="text" id="fullName" name="fullName" required />
+            <label htmlFor="fullName" className="form-label">
+              Your Name:
+            </label>
+            <input
+              type="text"
+              id="fullName"
+              name="fullName"
+              className="form-input"
+              required
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="serviceType">Service Type:</label>
-            <select id="serviceType" name="serviceType" required>
+            <label htmlFor="serviceType" className="form-label">
+              Service Type:
+            </label>
+            <select
+              id="serviceType"
+              name="serviceType"
+              className="form-select"
+              required
+            >
               <option value="">Select Service</option>
               <option value="itSupport">IT Support</option>
               <option value="softwareSupport">Software Support</option>
@@ -32,19 +55,32 @@ const StatementPage = () => {
             </select>
           </div>
           <div className="form-group">
-            <label htmlFor="budget">Budget:</label>
-            <input type="number" id="budget" name="budget" required />
+            <label htmlFor="budget" className="form-label">
+              Budget:
+            </label>
+            <input
+              type="number"
+              id="budget"
+              name="budget"
+              className="form-input"
+              required
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="description">Describe Your Needs:</label>
+            <label htmlFor="description" className="form-label">
+              Describe Your Needs:
+            </label>
             <textarea
               id="description"
               name="description"
               rows="4"
+              className="form-textarea"
               required
             ></textarea>
           </div>
-          <button type="submit">Submit Request</button>
+          <button type="submit" className="form-submit">
+            Submit Request
+          </button>
         </form>
       </div>
     </div>
